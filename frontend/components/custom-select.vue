c909e73bd13658ff80c93c99ded4087a863ae558
Tsuev <tsuevjudoka@gmail.com>
2023-05-26 11:24:32 +0300

    commit

---
+++ frontend/components/custom-select.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="custom__select"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
--- frontend/components/header.vue
+++ frontend/components/header.vue
@@ -1,13 +1,37 @@
 <template>
-  <div class="header">
+  <header class="header">
     <div class="main__nav">
       <div class="nav__logo">
         <img src="@/assets/images/logo.png" alt="" />
       </div>
+      <nav class="navigation">
+        <ul>
+          <li>Главная</li>
+          <li>Поставщики</li>
+          <li>Фермерам</li>
+          <li>О нас</li>
+        </ul>
+      </nav>
+      <div class="nav__city"></div>
     </div>
-  </div>
+  </header>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  padding: 10px 0px;
+  .main__nav {
+    @apply flex items-center;
+
+    .nav__logo {
+      width: 200px;
+    }
+
+    .navigation ul {
+      @apply flex;
+    }
+  }
+}
+</style>
