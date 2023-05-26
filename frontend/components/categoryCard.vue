0319925308b4f9abc05537bfd50dbf2466098729
Tsuev <tsuevjudoka@gmail.com>
2023-05-26 18:40:03 +0300

    commit

--- frontend/components/categoryCard.vue
+++ frontend/components/categoryCard.vue
@@ -1,37 +1,39 @@
- <template>
-  <div class="category"
-      v-bind:style="{ backgroundImage: 'url(' + objCategory.img + ')' }"
+<template>
+  <div
+    class="category"
+    @click="$router.push(`catalog/${objCategory._id}`)"
+    v-bind:style="{ backgroundImage: 'url(' + objCategory.img + ')' }"
   >
     <h2 class="category-title">{{ objCategory.title }}</h2>
   </div>
- </template>
+</template>
 
- <script setup>
-  const props = defineProps({
-    objCategory: Object,
-  })
+<script setup>
+const props = defineProps({
+  objCategory: Object,
+});
 </script>
 
- <style lang="scss" scoped>
-  .category{
-    width: 270px;
-    height: 270px;
-    border-radius: 16px;
-    cursor: pointer;
-    background-color: red;
-    transition: .5s;
-  }
+<style lang="scss" scoped>
+.category {
+  width: 270px;
+  height: 270px;
+  border-radius: 16px;
+  cursor: pointer;
+  background-color: red;
+  transition: 0.5s;
+}
 
-  .category:hover{
-    transform: scale(1.05);
-    transition: .5s;
-  }
+.category:hover {
+  transform: scale(1.05);
+  transition: 0.5s;
+}
 
-  .category-title{
-    padding: 15px;
-    font-size: 24px;
-    color: white;
-    font-weight: 600;
-    text-shadow: 1px 4px 20px black;
-  }
+.category-title {
+  padding: 15px;
+  font-size: 24px;
+  color: white;
+  font-weight: 600;
+  text-shadow: 1px 4px 20px black;
+}
 </style>
--- frontend/pages/catalog/index.vue
+++ frontend/pages/catalog/index.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="container">
-    <CategoryCard />
+    <div
+      v-if="categories"
+      class="catalog flex flex-wrap gap-4 justify-between py-5"
+    >
+      <CategoryCard
+        v-for="category in categories"
+        :key="category._id"
+        :objCategory="category"
+      />
+    </div>
+    <Spinner v-else />
   </div>
 </template>
 
 <script setup lang="ts">
-const categories = reactive([]);
+let categories: any = ref(null);
 
 fetch("http://192.168.88.151:3000/api/category/get-categories")
   .then((res) => res.json())
-  .then((res) => console.log(res));
+  .then((res) => (categories.value = res));
 </script>
 
 <style lang="scss" scoped></style>
