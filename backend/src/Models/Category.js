c1c4314feaf14cb83eab19d3726c2245a837ed92
superiorunknown5 <superiorunknown5@gmail.com>
2023-05-26 17:18:06 +0300

    category

--- backend/src/Models/Category.js
+++ backend/src/Models/Category.js
@@ -1,7 +1,7 @@
 import { Schema, model } from "mongoose";
 const category = new Schema({
     title: String,
-    image: {
+    img: {
         type: String,
         default: null,
     },
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import connect from './db/db.js'
 import Dotenv from 'dotenv'
+import categoryRouter from './Routers/CategoryRouter.js'
 const app = express()
 const port = 3000
 connect()
 Dotenv.config()
-app.get('/', (req, res) => {
-    res.send('Hello 12121212')
-})
+
+
+
+app.use(express.json())
+
+app.use('/category', categoryRouter);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
