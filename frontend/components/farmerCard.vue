1ba4e5d621dd38723195ccfcc2bfcdbee3808624
Tsuev <tsuevjudoka@gmail.com>
2023-05-27 13:24:11 +0300

    commit

--- frontend/components/farmerCard.vue
+++ frontend/components/farmerCard.vue
@@ -1,32 +1,34 @@
 <template>
-  <div class="farmer"
-        v-bind:style="{ backgroundImage: 'url(' + objFarmer.img + ')' }"
+  <div
+    class="farmer text-white text-bold"
+    v-bind:style="{ backgroundImage: 'url(' + objFarmer.img + ')' }"
   >
     <div class="farmer-card">
-      <div class="farmer-rate">
-        5
-      </div>
+      <div class="farmer-rate">Рейтинг фермера 1</div>
     </div>
     <div class="farmer-title">
-      <div class="farmer-title__name"> {{ objFarmer.title }} </div>
-      <div class="farmer-title__type"> <span> {{ objFarmer.subtitle }}:  </span> {{ objFarmer.farmType }}</div>
+      <div class="farmer-title__name">{{ objFarmer.title }}</div>
+      <div class="farmer-title__type">
+        <span> {{ objFarmer.subtitle }}: </span> {{ objFarmer.farmType }}
+      </div>
     </div>
-   
   </div>
 </template>
 
 <script setup>
-  defineProps({
-    objFarmer: Object
-  })
+defineProps({
+  objFarmer: Object,
+});
 </script>
 
-<style lang='scss' scoped>
-  .farmer{
-    width: 270px;
-    height: 330px;
-    border-radius: 16px;
-    padding: 15px;
-    background-color: green;
-  }
+<style lang="scss" scoped>
+.farmer {
+  text-shadow: 0 0 10px black;
+  width: 270px;
+  height: 330px;
+  border-radius: 16px;
+  padding: 15px;
+  background-color: green;
+  background-position: center;
+}
 </style>
