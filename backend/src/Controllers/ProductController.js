f6e9fed595b356814cf150c119795d2e3e8e5a0d
superiorunknown5 <superiorunknown5@gmail.com>
2023-05-27 10:04:00 +0300

    get prod by farm

--- backend/src/Controllers/ProductController.js
+++ backend/src/Controllers/ProductController.js
@@ -57,8 +57,6 @@ export const getProduct = async (req, res) => {
 
 export const getProductByCategory = async (req, res) => {
     let {categoryId} = req.query;
-   
-
     let product = await Product.find(        {
         categoryId,
     }).lean().catch(err => {
@@ -67,7 +65,6 @@ export const getProductByCategory = async (req, res) => {
             err
         })
     })
-
     let result = []
     for(let i = 0; i < product.length; i++) {
         let {name, surname} = await Farmer.findOne({
@@ -77,6 +74,27 @@ export const getProductByCategory = async (req, res) => {
         result.push({...product[i], farmerName:`${surname} ${name}`}) 
         
     }
+    res.json(result);
+}
 
+export const getProductByFarmer = async (req, res) => {
+    let {farmerId} = req.query;
+    let product = await Product.find(        {
+        farmerId,
+    }).lean().catch(err => {
+        res.json({
+            msg: 'Something wrong',
+            err
+        })
+    })
+    let result = []
+    for(let i = 0; i < product.length; i++) {
+        let {name, surname} = await Farmer.findOne({
+            _id: product[i].farmerId
+        })
+
+        result.push({...product[i], farmerName:`${surname} ${name}`}) 
+        
+    }
     res.json(result);
 }
--- backend/src/Routers/ProductRoutes.js
+++ backend/src/Routers/ProductRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
-import { addProduct, getProduct, getProductByCategory } from "../Controllers/ProductController.js";
+import { addProduct, getProduct, getProductByCategory, getProductByFarmer } from "../Controllers/ProductController.js";
 
 const router = Router();
 
 
 router.post('/add-product', addProduct)
 router.get('/get-product', getProduct)
-router.get('/get-product-category', getProductByCategory)
+router.get('/get-product-by-category', getProductByCategory)
+router.get('/get-product-by-farmer', getProductByFarmer)
 
 export default router;
